fe5b6de971f0545a4fadfd0972bdb63ff52c69f4
Muni Chaurasiya <199281592+Muni923@users.noreply.github.com>
2026-08-03 15:18:14 +0530

    fixed ai-controller

--- backend/src/controller/ai-controller.js
+++ backend/src/controller/ai-controller.js
@@ -4,14 +4,14 @@ const reviewCode = async (req, res) => {
     const { code, language } = req.body;
 
     if (!language && !code) {
-        res.status(400).json({ review: "Please select language and enter code..." });
+        return res.status(400).json({ review: "Please select language and enter code..." });
     }
     if (!code) {
-        res.status(400).json({ review: "Please paste your code..." });
+        return res.status(400).json({ review: "Please paste your code..." });
 
     }
     if (!language) {
-        res.status(400).json({ review: "Please select language..." });
+        return res.status(400).json({ review: "Please select language..." });
     }
     const ai = new GoogleGenAI({ apiKey: GEMINI_API_KEY });
 
@@ -20,6 +20,6 @@ const reviewCode = async (req, res) => {
         input: `You are an expert-level software developer, skilled in writing efficient,clean, and advanced coding.I'm sharing a piece of code written in ${language}.Your job is to deeply review this code and provide the following: quality rating: Better, Good, Norml, or Bad.Detailed suggestions for improvement, including best practices and advanced alternatives.A clear explanation of what the code does, step by step.A list of any potential bugs or logical errors, if found.Identification of syntax errors or runtime errors, if present.Solutions and recommendations on how to fix each identified issue and mention only about the code nothing else
       code:${code}`
     });
-     res.status(200).json({ review: interaction.output_text });
+    return res.status(200).json({ review: interaction.output_text });
 };
 module.exports = reviewCode;
